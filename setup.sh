ac2327e02b256ea8b40756b376cd0b4b6a4d6e12
AleHack <156984678+OFCALEXANDER@users.noreply.github.com>
2026-03-12 19:20:26 -0500

    Enhance setup.sh with Git installation and usage instructions
    Updated installation script to include Git and added instructions for running the application.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# Instalador automático para entornos Linux (Kali/Parrot/Arch)
+# Instalador automático para entornos Linux, nada de MACBOOK
 
 echo "[*] Instalando dependencias del sistema..."
-sudo apt update && sudo apt install -y tor proxychains4 python3-pip python3-venv
+sudo apt update && sudo apt install -y tor proxychains4 python3-pip python3-venv git
+
+echo "[*] Descargando Ghost Anonimus..."
+git clone https://github.com/OFCALEXANDER/Ghost-Anonimus.git
+cd Ghost-Anonimus || exit
 
 echo "[*] Configurando Tor ControlPort..."
 sudo bash -c 'grep -q "ControlPort 9051" /etc/tor/torrc || echo "ControlPort 9051" >> /etc/tor/torrc'
@@ -13,7 +17,11 @@ sudo service tor restart
 echo "[*] Creando entorno virtual..."
 python3 -m venv venv
 source venv/bin/activate
+
+echo "[*] Instalando dependencias Python..."
 pip install -r requirements.txt
 
 echo "[+] Configuración completada."
-echo "[!] Para ejecutar usa: source venv/bin/activate && python3 ghost_proxy.py"
+echo "[!] Para ejecutar usa:"
+echo "cd Ghost-Anonimus"
+echo "source venv/bin/activate && python3 ghost_proxy.py"
